db18f4e49434d97cccc2bce86fb9a2fb602ad835
Sandeep Singh Chouhan <sandeep.s.chouhan@intimetec.com>
2021-08-23 10:15:16 +0530

    changed the index file

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('This file is currently used for jenkins ci/cd');
+    res.send('Deployed nodejs container on centos machine');
 });
 
 app.listen('8081', () => {
